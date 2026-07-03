fd7dcc377e44faf4d54a16c34ae34650e1e13376
AnisaAisha <aa04049@st.habib.edu.pk>
2026-07-03 15:55:22 +0500

    Updated grid file  with texture mapping example

--- grid_taichi.py
+++ grid_taichi.py
@@ -1,4 +1,5 @@
 import taichi as ti
+from taichi.examples.patterns import taichi_logo
 
 ti.init(arch=ti.gpu)
 
@@ -8,10 +9,21 @@ grid_cols = 12
 grid_rows = 12
 spacing = 0.25
 num_triangles = (grid_rows - 1) * (grid_cols - 1) * 2
+num_vertices = grid_rows * grid_cols
 
 # Vertex and index arrays
-vertices = ti.Vector.field(3, dtype=ti.f32, shape=grid_rows * grid_cols)
+vertices = ti.Vector.field(3, dtype=ti.f32, shape=num_vertices)
 indices = ti.field(int, shape=num_triangles * 3)
+
+# UV array for texture mapping. One 2D texture coordinate (u, v) is saved per vertex
+uvs = ti.Vector.field(2, dtype=ti.f32, shape=num_vertices)
+
+# Colors array. One color value i.e. (R, G, B) is saved per vertex
+colors = ti.Vector.field(3, dtype=ti.f32, shape=num_vertices)
+
+# Texture initialization, k is the texture image dimension
+k = 256
+texture = ti.Texture(ti.Format.r32f, (k, k))
 
 # Generate vertices and space them out in the form of a grid
 @ti.kernel
@@ -39,9 +51,41 @@ def build_indices():
         indices[quad_id * 6 + 4] = base + 1
         indices[quad_id * 6 + 5] = base + grid_cols
 
+# Generate UV coordinates for each vertex in the grid according to grid dimensions
+@ti.kernel
+def build_uvs():
+    for i, j in ti.ndrange(grid_rows, grid_cols):
+        idx = i * grid_cols + j
+        u = j / (grid_cols - 1)
+        v = i / (grid_rows - 1)
+        uvs[idx] = ti.Vector([u, v])
+
+# Create textures from image. Note that here we are using the default Taichi logo
+# Think about how you could use any other image instead of the Taichi logo here
+@ti.kernel
+def make_texture(tex: ti.types.rw_texture(num_dimensions=2, fmt=ti.Format.r32f, lod=0), n: ti.i32):
+    for i, j in ti.ndrange(n, n):
+        ret = ti.cast(taichi_logo(ti.Vector([i, j]) / n), ti.f32)
+        tex.store(ti.Vector([i, j]), ti.Vector([ret, 0.0, 0.0, 0.0]))
+
+# Get the texture at each vertex's UV coordinate (stored in uvs array) and assign colors per-vertex
+@ti.kernel
+def sample_vertex_colors(tex: ti.types.texture(num_dimensions=2), n: ti.i32):
+    for idx in range(num_vertices):
+        u = uvs[idx][0]
+        v = uvs[idx][1]
+        uv = uvs[idx]
+        val = tex.fetch(ti.cast(uv * n, ti.i32), 0) # fetch the uv value, and interpolate texture image pixels on mesh
+        colors[idx] = ti.Vector([val.r, val.g, val.b])
+
 # Function calls to populate arrays
 build_vertices()
 build_indices()
+build_uvs()
+
+# Texture Functions
+make_texture(texture, k) # load texture
+sample_vertex_colors(texture, k) # assign texture to mesh
 
 # Taichi Initializations for 3D scene
 window = ti.ui.Window("plane mesh", RES)
@@ -50,6 +94,7 @@ scene  = ti.ui.Scene()
 camera = ti.ui.Camera()
 
 
+# Main GUI loop
 while window.running:
 
     # Camera cofigs
@@ -62,7 +107,8 @@ while window.running:
     scene.point_light(pos=(1.0, 2.0, 2.0), color=(1.0, 1.0, 1.0))
 
     # Create the mesh using vertex and index arrays we build earlier
-    scene.mesh(vertices, indices=indices, two_sided=True, color=(0.5, 0.42, 0.8))
+    # Now using per_vertex_color since we have per-vertex color information
+    scene.mesh(vertices, indices=indices, two_sided=True, per_vertex_color=colors)
 
     canvas.scene(scene)
     window.show()
